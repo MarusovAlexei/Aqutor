ad63cda198d646800c1f86332e8be026f336252b
Alexei <marusovalexei@gmail.com>
2023-04-24 17:46:22 +0300

    fix: fix animation

--- animation/burger.js
+++ animation/burger.js
@@ -1,7 +1,12 @@
 let menuBtn = document.querySelector(".menu-btn");
 let menu = document.querySelector(".header__nav-menu-items");
+let openMenu = document.querySelector(".body");
+
+console.log(openMenu);
+console.log(menuBtn);
 
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
+  openMenu.classList.toggle("open-menu");
 });
