dd92ef22389f83fd35533f0af6dbc6c3498fd149
Alexei <marusovalexei@gmail.com>
2023-04-24 19:31:24 +0300

    fix: fix size footer

--- animation/footer.js
+++ animation/footer.js
@@ -8,7 +8,7 @@ function generateBalls() {
     ball.classList.add("ball");
     gooeyAnimations.appendChild(ball);
 
-    var colors = ["#28323B", "#FFA036"];
+    var colors = ["#42307c", "#2992db"];
     ball.style.bottom = "0px";
     ball.style.left = Math.random() * windowWidth + "px";
     ball.style.animationDelay = Math.random() * 5 + "s";
